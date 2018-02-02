c75da507ff9172c114fe7c0d6576a1ee2a83cf8a
Jordan Marcus <jormarcus92@gmail.com>
2018-02-02 18:25:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
-}
-
-function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({}, object);
   return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
 }
